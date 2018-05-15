b56c73526d0dca901d817c7e33eddb9e62988c5c
Gideon le Grange <gideon@legrange.me>
2018-05-15 15:59:41 +0200

    Working on log control component

---
+++ src/main/java/me/legrange/log/LogControl.java
@@ -0,0 +1,75 @@
+package me.legrange.log;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+/**
+ * The component used to control logging on an application-wide level
+ *
+ * @author gideon
+ */
+public class LogControl {
+
+    public LogControl() throws FileNotFoundException {
+        if (System.out instanceof LogOutputStream) {
+            throw new IllegalStateException("Log control has already been installed");
+        }
+        System.setOut(new LogOutputStream());
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                // close files and shit 
+            }
+        });
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                // do some logging stuff yo
+                
+            }
+        });
+    }
+    
+    static LogControl getLogControl() { 
+        if (INSTANCE != null) {
+            
+        }
+    }
+
+    /**
+     * Set the logger for the package calling the logger.
+     *
+     * @param logger
+     */
+    public void setLogger(Logger logger) {
+        Router.getInstance().setLogger(logger);
+    }
+
+    /**
+     * Set the default application logger. Only call this from your
+     * application's main code. This must not be called by libraries.
+     *
+     * @param logger
+     */
+    public void setDefaultLogger(Logger logger) {
+        Router.getInstance().setDefaultLogger(logger);
+    }
+
+    /**
+     * Set the default logging level. Only call this from your application's
+     * main code. This must not be called by libraries.
+     *
+     * @param level The default level to set.
+     */
+    public void setDefaultLevel(Level level) {
+        Router.getInstance().setDefaultLevel(level);
+    }
+
+    private class LogOutputStream extends PrintStream {
+
+        public LogOutputStream() throws FileNotFoundException {
+            super("foo");
+        }
+
+    }
+
+}
