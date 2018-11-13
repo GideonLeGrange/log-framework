6d60abfb18181531ae3e13704807526cfe2d98e9
Gideon le Grange <gideon@legrange.me>
2018-11-13 11:27:32 +0200

    Implemented file logger and numbered exception logger

---
+++ src/main/java/za/co/adept/log/logger/FileLogger.java
@@ -0,0 +1,37 @@
+package za.co.adept.log.logger;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import static java.lang.String.format;
+import za.co.adept.log.Event;
+import za.co.adept.log.Logger;
+
+/**
+ *
+ * @author gideon
+ */
+public class FileLogger implements Logger {
+
+    private final PrintWriter out;
+
+    public FileLogger(String fileName) throws LoggerException {
+        out = openFile(fileName);
+    }
+
+    @Override
+    public void log(Event entry) {
+        out.printf("%s [%s]: %s\n", entry.getTimestamp(), entry.getLevel(), entry.getMessage());
+        out.flush();
+    }
+
+    protected final PrintWriter openFile(String fileName) throws LoggerException {
+        try {
+            return new PrintWriter(new FileWriter(fileName));
+        } catch (IOException ex) {
+            throw new LoggerException(format("Error opening log file '%s': %s", fileName, ex.getMessage()), ex);
+        }
+
+    }
+
+}
--- src/main/java/za/co/adept/log/logger/NumberedExceptionLogger.java
+++ src/main/java/za/co/adept/log/logger/NumberedExceptionLogger.java
@@ -1,41 +1,55 @@
 package za.co.adept.log.logger;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import static java.lang.String.format;
 import za.co.adept.log.Event;
 import za.co.adept.log.Logger;
 
 /**
- * A logger implementation that numbers exceptions and logs them with the number to the default logger, while 
- * writing the stack trace to a file.
- * 
+ * A logger implementation that numbers exceptions and logs them with the number
+ * to another logger, while writing the stack trace to a file.
+ *
  * @author gideon
  */
 public class NumberedExceptionLogger implements Logger {
 
-    private final String fileName;
-    private int msgId = 0; 
+    private int msgId = 0;
+    private final Logger logger;
+    private final PrintWriter out;
 
-    public NumberedExceptionLogger(String fileName) {
-        this.fileName = fileName;
+    public NumberedExceptionLogger(Logger logger, String exceptionFile) throws LoggerException {
+        this.logger = logger;
+        out = openFile(exceptionFile);
     }
-    
+
     @Override
-    public void log(Event entry) {
+    public synchronized void log(Event entry) {
         if (entry.getThrowable().isPresent()) {
             int id = nextId();
             entry = new Event(entry.getMessage() + format(" [%d]", id), entry.getTimestamp(), entry.getLevel(), entry.getThrowable().get());
-            writeToFile(id, entry.getThrowable().get());
+            writeToFile(id, entry);
         }
-//        Log.log(entry);
+        logger.log(entry);
     }
-    
-    private synchronized int nextId() { 
-        msgId ++;
+
+    private int nextId() {
+        msgId++;
         return msgId;
     }
-    
-    private void writeToFile(int id, Throwable ex) {
-        // put code to write to file here
-        
+
+    private void writeToFile(int id, Event entry) {
+        out.printf("[#%d - %s]\n", msgId, entry.getTimestamp());
+        entry.getThrowable().get().printStackTrace(out);
+    }
+
+    protected final PrintWriter openFile(String fileName) throws LoggerException {
+        try {
+            return new PrintWriter(new FileWriter(fileName));
+        } catch (IOException ex) {
+            throw new LoggerException(format("Error opening log file '%s': %s", fileName, ex.getMessage()), ex);
+        }
+
     }
 }
