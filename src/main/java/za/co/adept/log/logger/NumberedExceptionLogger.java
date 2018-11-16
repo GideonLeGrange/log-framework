acf4f9225d099e273a0d5405220c4c6d66c3e607
Gideon le Grange <gideon@legrange.me>
2018-11-16 14:00:06 +0200

    Changed to use uuid for numbered exception

--- src/main/java/za/co/adept/log/logger/NumberedExceptionLogger.java
+++ src/main/java/za/co/adept/log/logger/NumberedExceptionLogger.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import static java.lang.String.format;
+import java.util.UUID;
 import za.co.adept.log.Event;
 import za.co.adept.log.Logger;
 
@@ -15,7 +16,6 @@ import za.co.adept.log.Logger;
  */
 public class NumberedExceptionLogger implements Logger {
 
-    private int msgId = 0;
     private final Logger logger;
     private final PrintWriter out;
 
@@ -27,20 +27,19 @@ public class NumberedExceptionLogger implements Logger {
     @Override
     public synchronized void log(Event entry) {
         if (entry.getThrowable().isPresent()) {
-            int id = nextId();
-            entry = new Event(entry.getMessage() + format(" [%d]", id), entry.getTimestamp(), entry.getLevel(), entry.getThrowable().get());
+            String id = getId();
+            entry = new Event(entry.getMessage() + format(" [%s]", id), entry.getTimestamp(), entry.getLevel(), entry.getThrowable().get());
             writeToFile(id, entry);
         }
         logger.log(entry);
     }
 
-    private int nextId() {
-        msgId++;
-        return msgId;
+    private String getId() {
+        return UUID.randomUUID().toString();
     }
 
-    private void writeToFile(int id, Event entry) {
-        out.printf("[#%d - %s]\n", msgId, entry.getTimestamp());
+    private void writeToFile(String id, Event entry) {
+        out.printf("[#%s - %s]\n", msgId, entry.getTimestamp());
         entry.getThrowable().get().printStackTrace(out);
         out.flush();
     }
