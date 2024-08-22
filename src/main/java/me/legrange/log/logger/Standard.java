e0e36dadf4c5428d6029f5b20101aaa5ac771308
Gideon Le Grange <gideon@legrange.me>
2024-08-22 17:33:48 +0200

    \r (Bumped to 1.4.1)

--- src/main/java/me/legrange/log/logger/Standard.java
+++ src/main/java/me/legrange/log/logger/Standard.java
@@ -5,7 +5,7 @@ import me.legrange.log.Event;
 final class Standard {
 
     static String format(Event entry, boolean useEmoji) {
-        return String.format("%s %s %s\n",
+        return String.format("\r%s %s %s\n",
                 entry.getTimestamp(),
                 useEmoji ? entry.getLevel().emoji() : "[" + entry.getLevel().name() + "]",
                 entry.getMessage());
